d4d955663d0407f606b3e3bb0d08f8388df1ccbe
clbDream <3214341986@qq.com>
2022-10-20 19:55:28 +0800

    1

--- app/src/main/java/com/clb/recordthings_android/ui/activity/MainActivity.kt
+++ app/src/main/java/com/clb/recordthings_android/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.clb.recordthings_android.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
